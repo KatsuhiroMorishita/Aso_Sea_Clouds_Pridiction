9203d946b1cdb6b6b7495011c12cc53f92f7606e
morishita <morimori.ynct@gmail.com>
2015-11-01 19:37:39 +0900

    天気予報を更新された時のみ保存するように改めた
    気象庁の天気予報を1時間毎に保存して確認したが、更新タイミングが毎日まちまちなようだ。そこで、ハッシュ値を比較することで更新データが手に入った時のみファイルに保存する様に仕様を変更した。

--- weather/weather_prediction_downloader.py
+++ weather/weather_prediction_downloader.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 気象庁の天気予報を定期的にpandasでダウンロードする
+# purpose: 気象庁の天気予報を定期的にpandasでダウンロードする
+# created: 2015-10-
+# license: MIT
 import pandas
 from datetime import datetime as dt
 from datetime import timedelta as td
 import time
 
+# ダウンロードする時刻をセット　ここでは、過去も含む
 hours = []
-for i in range(24):
-	hours.append(td(hours=i, minutes=1))
+for i in range(24): # 気象庁の予報はいつ更新されるか分からない・・・
+	hours.append(td(hours=i, minutes=1, seconds=0))
+# 次にダウンロードすべき時刻　過去の時刻は全て未来の時刻に更新
 _next = []
 for mem in hours:
 	t = dt(dt.now().year, dt.now().month, dt.now().day, 0, 0, 0) + mem
@@ -18,15 +22,25 @@ for mem in hours:
 
 
 while True:
+	_hash = None
 	for i in range(len(_next)):
 		t = _next[i]
 		now = dt.now()
 		if t <= now:
-			print("--download--")
+			print("--try download--")
 			url = "http://www.jma.go.jp/jp/week/"
-			fetched_dataframes = pandas.io.html.read_html(url)
-			table = fetched_dataframes[5]
-			table.to_csv("wether_" + now.strftime('%Y_%m_%d_%H%M%S') + ".csv", encoding="utf-8-sig")
+			try:
+				fetched_dataframes = pandas.io.html.read_html(url) # download
+				table = fetched_dataframes[5]
+				if _hash != hash(str(table)):                      # 同じ情報は保存しない
+					print("--save--")
+					table.to_csv("wether_" + now.strftime('%Y_%m_%d_%H%M%S') + ".csv", encoding="utf-8-sig")
+					_hash = hash(str(table))
+				else:
+					print("--same data downloaded--")
+			except Exception as e:
+				print("--error--")
+				print(str(e))
 			_next[i] = t + td(days=1)
 	print(_next)
 	time.sleep(60)
