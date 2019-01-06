c3fb925dd4bea64b3de35bec3558b1ea7743b654
Katsuhiro Morishita <morimori.ynct@gmail.com>
2019-01-07 00:02:10 +0900

    インデントのタブを半角スペースに切り替え

--- machine.py
+++ machine.py
@@ -32,48 +32,48 @@ def create_dir(path_list):
 
 
 def _get_new_path():
-	""" 保存する際に利用できるファイル名・フォルダ名を返すジェネレータ
-	"""
-	i = 0
-	while True:
-		_path = "av_entry_temp_{0:05d}.cmp".format(i)
-		i += 1
-		yield _path
+    """ 保存する際に利用できるファイル名・フォルダ名を返すジェネレータ
+    """
+    i = 0
+    while True:
+        _path = "av_entry_temp_{0:05d}.cmp".format(i)
+        i += 1
+        yield _path
 
 
 def new():
-	""" 初期化した機械学習オブジェクトを返す
-	"""
-	clf = ml(max_features="auto", max_depth=40, n_estimators=150, n_jobs=2) # RF
-	return clf
+    """ 初期化した機械学習オブジェクトを返す
+    """
+    clf = ml(max_features="auto", max_depth=40, n_estimators=150, n_jobs=2) # RF
+    return clf
 
 
 path_generator = _get_new_path()
 def save(clf, dir_path):
-	""" 機械学習オブジェクトを保存する
-	"""
-	print(dir_path)
-	create_dir([dir_path])
-	_fpath = dir_path + "/" + next(path_generator)
-	joblib.dump(clf, _fpath, compress=True)
-	return
+    """ 機械学習オブジェクトを保存する
+    """
+    print(dir_path)
+    create_dir([dir_path])
+    _fpath = dir_path + "/" + next(path_generator)
+    joblib.dump(clf, _fpath, compress=True)
+    return
 
 
 def load(path):
-	""" 機械学習オブジェクトをファイルから復元する
-	"""
-	clf = joblib.load(path)        # 機械学習オブジェクト復元
-	return clf
+    """ 機械学習オブジェクトをファイルから復元する
+    """
+    clf = joblib.load(path)        # 機械学習オブジェクト復元
+    return clf
 
 
 def get_path_list(base):
-	""" 機械学習オブジェクトをのファイルまたはフォルダ一覧を取得する
-	"""
-	#_group = set(["gaph.pgtxt", "model-200.meta", "", "", "", ""]) # ファイル内容の凶器を使うならと思ったけど面倒なので放置
-	flist = glob.glob(base + "/*.cmp")
-	ans = []
-	for fpath in flist:
-		if os.path.isfile(fpath):
-			ans.append(fpath)
+    """ 機械学習オブジェクトをのファイルまたはフォルダ一覧を取得する
+    """
+    #_group = set(["gaph.pgtxt", "model-200.meta", "", "", "", ""]) # ファイル内容の凶器を使うならと思ったけど面倒なので放置
+    flist = glob.glob(base + "/*.cmp")
+    ans = []
+    for fpath in flist:
+        if os.path.isfile(fpath):
+            ans.append(fpath)
 
-	return ans
+    return ans
